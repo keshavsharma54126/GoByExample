1688b9d139b64e8b25cf86a70ebd0192e19f1e2c
Keshav sharma <sharmakeshav54126@gmail.com>
2025-01-30 18:01:20 +0000

    variatdic function using slices

--- slices/slices.go
+++ slices/slices.go
@@ -5,6 +5,15 @@ import (
 	"slices"
 )
 
+func sum (nums ...float64) float64{
+	total := 0.0
+	for i:=0;i<len(nums);i++{
+		total+= nums[i]
+	}
+	return total
+
+}
+
 func main() {
 
 	var s []string
