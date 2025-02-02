91ecfb0505f5af4d20ac40dac503aeae3c682bc2
Keshav sharma <sharmakeshav54126@gmail.com>
2025-02-02 16:58:39 +0000

    currying

--- advancedFunctions/advFunctions.go
+++ advancedFunctions/advFunctions.go
@@ -16,8 +16,19 @@ func mul(x,y int)int{
 func aggregator(a,b,c int,arithmetic func(int,int)int)int{
 	return arithmetic(arithmetic(a,b),c)
 }
+//currying (taking a function as an input and returing a function as an input)
+func selfMath(mathFunc func(int,int) int) func(int)int{
+	return func(x int) int{
+		return mathFunc(x,x)
+	}
+}
 
 func main(){
 	fmt.Println(aggregator(2,3,5,add))
 	fmt.Println(aggregator(2,3,5,mul))
+	squareFunc:= selfMath(mul)
+	doubleFunc:= selfMath(add)
+
+	fmt.Println(squareFunc(3))
+	fmt.Println(doubleFunc(4))
 }
