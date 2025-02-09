acdf5970f4cdc211fe8fb309b81c9a318b28884a
Keshav sharma <sharmakeshav54126@gmail.com>
2025-02-09 12:01:35 +0000

    mutexes in go

---
+++ mutexes/mutexes.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type safeCounter struct {
+	counts map[string]int
+	mux    *sync.Mutex
+	mux2   *sync.RWMutex
+}
+
+
+func (sc safeCounter) inc(key string) {
+	sc.mux.Lock()
+	defer sc.mux.Unlock()
+	sc.slowIncrement(key)
+}
+func (sc safeCounter) val(key string) int {
+	sc.mux.Lock()
+	defer sc.mux.Unlock()
+	return sc.counts[key]
+}
+func (sc safeCounter) slowIncrement(key string) {
+	tempCounter := sc.counts[key]
+	time.Sleep(time.Microsecond)
+	tempCounter++
+	sc.counts[key] = tempCounter
+}
+
+// RW Mutex(it can give you read and write locak and unlock also along with lock and unlock)
+func(sc safeCounter) rval(key string) int{}
+
+func main() {
+	sc := safeCounter{
+		counts: make(map[string]int),
+		mux:    &sync.Mutex{},
+	}
+	fmt.Println(sc.counts["sharmakeshav54126@gmail.com"])
+	sc.inc("keshav.sharma@novostack.com")
+	val := sc.val("keshav.sharma@novostack.com")
+	fmt.Println(val)
+}
